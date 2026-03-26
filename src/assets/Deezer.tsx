3afa4436fccbf35f4f1e3fc230b0f285b0b0073f
stephaniebrandao <salbuquerque.md@gmail.com>
2026-03-26 20:08:00 +0000

    create new components (as assets) to visually improve the webpage

---
+++ src/assets/Deezer.tsx
@@ -0,0 +1,5 @@
+function Deezer () {
+    return <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#3C4A28" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-music-icon lucide-music"><path d="M9 18V5l12-2v13"/><circle cx="6" cy="18" r="3"/><circle cx="18" cy="16" r="3"/></svg>
+}
+
+export default Deezer;
