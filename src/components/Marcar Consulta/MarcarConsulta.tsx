6b84189c2f9a5807521693d32b173c1d26cd64c6
stephaniebrandao <salbuquerque.md@gmail.com>
2026-03-30 13:58:16 +0100

    add MarcarConsulta component with form and styling

--- src/components/Marcar Consulta/MarcarConsulta.tsx
+++ src/components/Marcar Consulta/MarcarConsulta.tsx
@@ -1,44 +1,57 @@
+import Button from "../Button/Button";
+import "./marcarConsulta.css";
+
 function MarcarConsulta() {
   return (
     <>
-      <header>
+      <header className="header-consulta">
         <h1>Marque aqui a sua consulta!</h1>
         <h3>
-          Marque aqui a sua consulta na Flore Odontológica. A sua marcação será
+          Marque aqui a sua consulta na Flore Odontológica. <br/> A sua marcação será
           confirmada por e-mail e/ou mensagem.
         </h3>
       </header>
 
       <section>
-        <form action="#" method="post">
+        <div className="secao-marcacao">
+          <form action="#" method="post">
             <label htmlFor="name">Nome:</label>
-            <input type="text" required/>
-
+            <input type="text" required />
 
             <label htmlFor="email">E-mail:</label>
-            <input type="email" required/>
-
+            <input type="email" required />
 
             <label htmlFor="telefone">Telefone:</label>
-            <input type="tel" required/>
+            <input type="tel" required />
 
             <label htmlFor="especialidade">Especialidade:</label>
             <select name="" id="" required>
-                <option value=""></option>
+              <option value=""></option>
             </select>
             {/*código para mostrar especialidades de forma dinâmica*/}
 
             <label htmlFor="date">Data da consulta:</label>
-            <input type="date" required/>
+            <input type="date" required />
             {/*código para mostrar apenas datas disponíveis*/}
 
             <label htmlFor="time">Horário da consulta:</label>
-            <input type="time" required/>
+            <input type="time" required />
             {/*código para mostrar apenas horários disponíveis*/}
 
             <label htmlFor="mensagem">Mensagem:</label>
-            <textarea name="mensagem" id="" placeholder="Mensagem para a equipe da clínica (opcional): sintomas, dúvidas ou observações..." />
-        </form>
+            <textarea
+              name="mensagem"
+              id=""
+              placeholder="Mensagem para a equipe da clínica (opcional): sintomas, dúvidas ou observações..."
+            />
+
+            <div className="btn-form">
+              <Button variant={"nav"} isActive={false}>
+                Enviar
+              </Button>
+            </div>
+          </form>
+        </div>
       </section>
     </>
   );
