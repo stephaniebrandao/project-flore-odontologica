3873c4e650ecc1df805305123c208e29a430ddd6
stephaniebrandao <salbuquerque.md@gmail.com>
2026-03-31 13:57:13 +0100

    feat: implement Especialidades component with API integration and styling

--- src/components/Especialidades/Especialidades.tsx
+++ src/components/Especialidades/Especialidades.tsx
@@ -1,5 +1,58 @@
-function Especialidades () {
-    return <h1>Especialidades</h1>
+import { useEffect, useState } from "react";
+import { supabase } from "../../supabaseClient";
+import "./especialidades.css";
+import type { IEspecialidades } from "../../types";
+import Card from "../Card/Card";
+import InfoFlore from "../Info-Flore/InfoFlore";
+
+function Especialidades() {
+  const [especialidades, setEspecialidades] = useState<IEspecialidades[]>([]);
+  console.log("especialidades", especialidades);
+
+  useEffect(() => {
+    const fetchEspecialidades = async () => {
+      const { data: newEspecialidades, error } = await supabase
+        .from("especialidades")
+        .select();
+
+      if (error) console.error("Error fetching:", error);
+      console.log("newEspecialidades", newEspecialidades);
+      setEspecialidades(newEspecialidades || []);
+    };
+
+    fetchEspecialidades();
+  }, []);
+
+  return (
+    <>
+      <div className="title-specs">
+        <h1>Especialidades Focadas na Sua Saúde</h1>
+        <p>
+          Explore como cada uma de nossas especialidades pode transformar o seu
+          bem-estar.
+        </p>
+        <p> Estamos prontos para cuidar de si!</p>
+        <aside>Venha florescer conosco!</aside>
+      </div>
+      <div className="especialidades-api">
+        {especialidades.map((especialidade) => (
+          <Card
+            image={
+              <img
+                style={{ width: "100px", height: "100px", borderRadius: "50%" }}
+                src={especialidade.imagem}
+              />
+            }
+            title={especialidade.nome}
+            subtitle={especialidade.descricao}
+            variant={"specs"}
+          />
+        ))}
+      </div>
+
+      <InfoFlore />
+    </>
+  );
 }
 
 export default Especialidades;
