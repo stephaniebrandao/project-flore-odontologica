ab959d1e9c9a6a6881f72a51ead9bf5240caa25b
stephaniebrandao <salbuquerque.md@gmail.com>
2026-03-31 16:40:41 +0100

    refactor: enhance MarcarConsulta component with form submission handling and dynamic especialidades and horarios

--- src/components/Marcar Consulta/MarcarConsulta.tsx
+++ src/components/Marcar Consulta/MarcarConsulta.tsx
@@ -1,34 +1,87 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Button from "../Button/Button";
 import "./marcarConsulta.css";
 import { supabase } from "../../supabaseClient";
-
+import type { Horarios, IEspecialidades } from "../../types";
 
 function MarcarConsulta() {
-  const [newTask, setNewTask] = useState({
+  const [newMarcacao, setNewMarcacao] = useState({
     nome: "",
     email: "",
     telefone: 0,
     mensagem: "",
+    especialidade: "",
+    horarios: "",
   });
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
 
-    const { error } = await supabase.from("tasks").insert([newTask]);
+    const { error } = await supabase.from("marcacao").insert([newMarcacao]);
 
     if (error) {
-      console.error("Error adding task: ", error.message);
-      return;
+      throw new Error(error.message);
     }
 
-    setNewTask({
+    setNewMarcacao({
       nome: "",
       email: "",
       telefone: 0,
       mensagem: "",
+      especialidade: "",
+      horarios: "",
     });
   };
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleButtonSubmit = async (e: any) => {
+    e.preventDefault();
+
+    if (isSubmitting) return;
+
+    setIsSubmitting(true);
+
+    try {
+      await handleSubmit(e);
+      alert("Consulta marcada!");
+    } catch (error) {
+      alert("Tente novamente!");
+      setIsSubmitting(false);
+    }
+  };
+
+  // -----------------
+
+  const [especialidades, setEspecialidades] = useState<IEspecialidades[]>([]);
+  console.log("especialidades", especialidades);
+
+  useEffect(() => {
+    const fetchEspecialidades = async () => {
+      const { data: newEspecialidades, error } = await supabase
+        .from("especialidades")
+        .select();
+
+      if (error) console.error("Error fetching:", error);
+      console.log("newEspecialidades", newEspecialidades);
+      setEspecialidades(newEspecialidades || []);
+    };
+
+    fetchEspecialidades();
+  }, []);
+
+  const [horarios, setHorarios] = useState<Horarios[]>([]);
+
+  useEffect(() => {
+    const fetchHorarios = async () => {
+      const { data: newHorarios } = await supabase.from("horarios").select();
+
+      setHorarios(newHorarios || []);
+    };
+
+    fetchHorarios();
+  }, []);
+
 
   return (
     <>
@@ -42,50 +95,78 @@ function MarcarConsulta() {
 
       <section>
         <div className="secao-marcacao">
-          <form onSubmit={handleSubmit} action="#" method="post">
+          <form onSubmit={handleButtonSubmit} action="#" method="post">
             <label htmlFor="name">Nome:</label>
             <input
               type="text"
               placeholder="Ana Silva"
               onChange={(e) =>
-                setNewTask((prev) => ({ ...prev, nome: e.target.value }))
+                setNewMarcacao((prev) => ({ ...prev, nome: e.target.value }))
               }
               required
             />
-
             <label htmlFor="email">E-mail:</label>
             <input
               type="email"
               placeholder="seu@email.com"
               onChange={(e) =>
-                setNewTask((prev) => ({ ...prev, email: e.target.value }))
+                setNewMarcacao((prev) => ({ ...prev, email: e.target.value }))
               }
               required
             />
-
             <label htmlFor="telefone">Telefone:</label>
             <input
               type="tel"
               placeholder="83 99000-9000"
               onChange={(e) =>
-                setNewTask((prev) => ({ ...prev, telefone: Number(e.target.value) }))
+                setNewMarcacao((prev) => ({
+                  ...prev,
+                  telefone: Number(e.target.value),
+                }))
               }
               required
             />
-
-            {/* <label htmlFor="especialidade">Especialidade:</label>
-            <select name="" id="" required>
-              <option value=""></option>
+            <label htmlFor="especialidade">Especialidade:</label>
+            <select
+              name="especialidades"
+              id="especialidades"
+              onChange={(e) =>
+                setNewMarcacao((prev) => ({
+                  ...prev,
+                  especialidade: e.target.value,
+                }))
+              }
+              required
+            >
+              <option value="">Selecione uma especialidade</option>
+              {especialidades.map((especialidade) => (
+                <option key={especialidade.id} value={especialidade.nome}>
+                  {especialidade.nome}
+                </option>
+              ))}
             </select>
-            código para mostrar especialidades de forma dinâmica */}
-
-            {/* <label htmlFor="date">Data da consulta:</label>
+            <label htmlFor="date">Data da consulta:</label>
             <input type="date" required />
-            {/*código para mostrar apenas datas disponíveis*/}
 
-            {/* <label htmlFor="time">Horário da consulta:</label>
-            <input type="time" required />
-            código para mostrar apenas horários disponíveis */}
+            <label htmlFor="horarios">Horários:</label>
+            <select
+              name="horarios"
+              id="horarios"
+              onChange={(e) =>
+                setNewMarcacao((prev) => ({
+                  ...prev,
+                  horarios: e.target.value,
+                }))
+              }
+              required
+            >
+              <option value="">Selecione um horário</option>
+              {horarios.map((horario) => (
+                <option key={horario.id} value={horario.hora}>
+                  {horario.hora}
+                </option>
+              ))}
+            </select>
 
             <label htmlFor="mensagem">Mensagem:</label>
             <textarea
@@ -93,12 +174,14 @@ function MarcarConsulta() {
               id=""
               placeholder="Mensagem para a equipe da clínica (opcional): sintomas, dúvidas ou observações..."
               onChange={(e) =>
-                setNewTask((prev) => ({ ...prev, mensagem: e.target.value }))
+                setNewMarcacao((prev) => ({
+                  ...prev,
+                  mensagem: e.target.value,
+                }))
               }
             />
-
             <div className="btn-form">
-              <Button variant={"nav"} isActive={false}>
+              <Button variant={"nav"} isActive={true} isDisabled={isSubmitting}>
                 Enviar
               </Button>
             </div>
