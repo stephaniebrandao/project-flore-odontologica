4bde23ddee819bb86ae5e47e6ca86aa0682ac67b
stephaniebrandao <salbuquerque.md@gmail.com>
2026-03-31 13:51:58 +0100

    feat: implement form for scheduling consultations with Supabase integration

--- src/components/Marcar Consulta/MarcarConsulta.tsx
+++ src/components/Marcar Consulta/MarcarConsulta.tsx
@@ -1,48 +1,100 @@
+import { useState } from "react";
 import Button from "../Button/Button";
 import "./marcarConsulta.css";
+import { supabase } from "../../supabaseClient";
+
 
 function MarcarConsulta() {
+  const [newTask, setNewTask] = useState({
+    nome: "",
+    email: "",
+    telefone: 0,
+    mensagem: "",
+  });
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    const { error } = await supabase.from("tasks").insert([newTask]);
+
+    if (error) {
+      console.error("Error adding task: ", error.message);
+      return;
+    }
+
+    setNewTask({
+      nome: "",
+      email: "",
+      telefone: 0,
+      mensagem: "",
+    });
+  };
+
   return (
     <>
       <header className="header-consulta">
         <h1>Marque aqui a sua consulta!</h1>
         <h3>
-          Marque aqui a sua consulta na Flore Odontológica. <br/> A sua marcação será
-          confirmada por e-mail e/ou mensagem.
+          Marque aqui a sua consulta na Flore Odontológica. <br /> A sua
+          marcação será confirmada por e-mail e/ou mensagem.
         </h3>
       </header>
 
       <section>
         <div className="secao-marcacao">
-          <form action="#" method="post">
+          <form onSubmit={handleSubmit} action="#" method="post">
             <label htmlFor="name">Nome:</label>
-            <input type="text" required />
+            <input
+              type="text"
+              placeholder="Ana Silva"
+              onChange={(e) =>
+                setNewTask((prev) => ({ ...prev, nome: e.target.value }))
+              }
+              required
+            />
 
             <label htmlFor="email">E-mail:</label>
-            <input type="email" required />
+            <input
+              type="email"
+              placeholder="seu@email.com"
+              onChange={(e) =>
+                setNewTask((prev) => ({ ...prev, email: e.target.value }))
+              }
+              required
+            />
 
             <label htmlFor="telefone">Telefone:</label>
-            <input type="tel" required />
+            <input
+              type="tel"
+              placeholder="83 99000-9000"
+              onChange={(e) =>
+                setNewTask((prev) => ({ ...prev, telefone: Number(e.target.value) }))
+              }
+              required
+            />
 
-            <label htmlFor="especialidade">Especialidade:</label>
+            {/* <label htmlFor="especialidade">Especialidade:</label>
             <select name="" id="" required>
               <option value=""></option>
             </select>
-            {/*código para mostrar especialidades de forma dinâmica*/}
+            código para mostrar especialidades de forma dinâmica */}
 
-            <label htmlFor="date">Data da consulta:</label>
+            {/* <label htmlFor="date">Data da consulta:</label>
             <input type="date" required />
             {/*código para mostrar apenas datas disponíveis*/}
 
-            <label htmlFor="time">Horário da consulta:</label>
+            {/* <label htmlFor="time">Horário da consulta:</label>
             <input type="time" required />
-            {/*código para mostrar apenas horários disponíveis*/}
+            código para mostrar apenas horários disponíveis */}
 
             <label htmlFor="mensagem">Mensagem:</label>
             <textarea
               name="mensagem"
               id=""
               placeholder="Mensagem para a equipe da clínica (opcional): sintomas, dúvidas ou observações..."
+              onChange={(e) =>
+                setNewTask((prev) => ({ ...prev, mensagem: e.target.value }))
+              }
             />
 
             <div className="btn-form">
