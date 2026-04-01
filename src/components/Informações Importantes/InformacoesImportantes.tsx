0ea70f0349a184b4f95e779ec24c1163efc050e6
stephaniebrandao <salbuquerque.md@gmail.com>
2026-04-01 23:57:09 +0100

    feat: improve InformacoesImportantes component with 2 other components (Header and Accordion) and update imports

--- src/components/Informações Importantes/InformacoesImportantes.tsx
+++ src/components/Informações Importantes/InformacoesImportantes.tsx
@@ -1,5 +1,25 @@
-function InformacoesImportantes () {
-    return <h1>Informações Importantes</h1>
+import info from "../../data";
+import Accordion from "../Accordion/Accordion";
+import Header from "../Header/Header";
+import "./informacoesImportantes.css";
+
+function InformacoesImportantes() {
+  const subtitle =
+    "O seu bem-estar começa antes mesmo da consulta. Saiba como nos organizamos para cuidar de si.";
+
+  return (
+    <>
+      <Header
+        title={"Informações Importantes"}
+        subtitle={subtitle}
+        variant={"first"}
+      />
+
+      <div className="container">
+        <Accordion data={info} />
+      </div>
+    </>
+  );
 }
 
 export default InformacoesImportantes;
