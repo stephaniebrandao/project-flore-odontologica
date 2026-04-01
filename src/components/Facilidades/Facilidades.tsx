ad29f0ef7dc5c16fbaea67838d6c259dc0dac5c4
stephaniebrandao <salbuquerque.md@gmail.com>
2026-04-02 00:00:07 +0100

    feat: implement Facilidades page with Supabase data fetching and update imports

--- src/components/Facilidades/Facilidades.tsx
+++ src/components/Facilidades/Facilidades.tsx
@@ -1,5 +1,48 @@
-function Facilidades () {
-    return <h1>Facilidades</h1>
+import { useEffect, useState } from "react";
+import "./facilidades.css";
+import { supabase } from "../../supabaseClient";
+import Header from "../Header/Header";
+
+function Facilidades() {
+  const [convenios, setConvenios] = useState<{ nome: string }[]>([]);
+
+  useEffect(() => {
+    const fetchConvenios = async () => {
+      const { data: newConvenios } = await supabase.from("convenios").select();
+
+      setConvenios(newConvenios || []);
+    };
+
+    fetchConvenios();
+  }, []);
+
+  const subtitle = "Na Flore, acreditamos que um sorriso saudável deve ser acessível. Por isso, oferecemos diversas modalidades de atendimento e parcerias com os principais planos de saúde."
+
+  return (
+    <>
+
+    <Header title={"Facilidades e Convênios"} subtitle={subtitle} variant={"first"}/>
+
+        <ol className="convenios-particular">
+          <li>Planos de Saúde (Convênios)</li>
+          <p>
+            Trabalhamos com uma ampla rede credenciada para garantir que você
+            utilize os benefícios do seu plano com a excelência do nosso
+            atendimento.
+          </p>
+          <ul>
+            {convenios.map((convenio, index) => (
+              <li key={index}>{convenio.nome}</li>
+            ))}
+          </ul>
+          <li>Atendimento Particular</li>
+          <p className="pagamentos">
+            Aceitamos cartões de crédito (parcelamento disponível), débito, PIX
+            e numerário.
+          </p>
+        </ol>
+    </>
+  );
 }
 
 export default Facilidades;
