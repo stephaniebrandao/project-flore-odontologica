e0ae35a997a405274baa6ac564510536a5fad484
stephaniebrandao <salbuquerque.md@gmail.com>
2026-04-01 23:58:16 +0100

    feat: create Header component with title and subtitle props and update imports

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,20 @@
+import "./header.css";
+
+interface IHeader {
+  title: string;
+  subtitle: string;
+  variant: "first";
+}
+
+function Header({ title, subtitle, variant }: IHeader) {
+  return (
+    <>
+      <div className={`${variant}`}>
+        <h1>{title}</h1>
+        <p>{subtitle}</p>
+      </div>
+    </>
+  );
+}
+
+export default Header;
