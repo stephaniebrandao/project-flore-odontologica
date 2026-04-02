1ee5a234f3861dc88c152649510323e6697c47bd
stephaniebrandao <salbuquerque.md@gmail.com>
2026-04-02 21:11:48 +0100

    refactor: update dr. Kalygia photo source to imported module in App

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Button from "./components/Button/Button";
 import InsideBanner from "./components/InsideBanner/InsideBanner";
 import "./App.css";
 import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
+import { Link } from "react-router";
+import Kalygia from "./components/Images/photo_kalygia.jpg"
 
 function App() {
   const POSICAO_CLINICA = { lat: -7.167731, lng: -34.830455 };
@@ -26,48 +28,50 @@ function App() {
   return (
     <>
       <section className="container-index-flore">
-          <div>
-            <img
-              className="kalygia-flore-img"
-              src="src/components/Images/photo_kalygia.jpg"
-              alt="Dra. Kalygia Cavalcanti - diretora clínica"
-              width={205}
-              height={300}
-            />
-          </div>
-          <div className="intro-flore">
-            <h1>Sua jornada para um sorriso radiante começa aqui!</h1>
-            <p>
-              Na Flore Odontológica, acreditamos que cada sorriso carrega uma
-              história única — e é nossa missão cuidar de cada uma com
-              dedicação, precisão e carinho.
-            </p>
+        <div>
+          <img
+            className="kalygia-flore-img"
+            src={Kalygia}
+            alt="Dra. Kalygia Cavalcanti - diretora clínica"
+            width={205}
+            height={300}
+          />
+        </div>
+        <div className="intro-flore">
+          <h1>Sua jornada para um sorriso radiante começa aqui!</h1>
+          <p>
+            Na Flore Odontológica, acreditamos que cada sorriso carrega uma
+            história única — e é nossa missão cuidar de cada uma com dedicação,
+            precisão e carinho.
+          </p>
 
-            <p>
-              Sob a direção clínica da Dra. Kalygia Cavalcanti, nossa equipe une
-              tecnologia de ponta, atendimento humanizado e uma abordagem
-              personalizada para transformar a experiência odontológica em algo
-              leve, seguro e inspirador.
-            </p>
+          <p>
+            Sob a direção clínica da Dra. Kalygia Cavalcanti, nossa equipe une
+            tecnologia de ponta, atendimento humanizado e uma abordagem
+            personalizada para transformar a experiência odontológica em algo
+            leve, seguro e inspirador.
+          </p>
 
-            <p>
-              A primeira consulta é o início dessa jornada: um momento de
-              escuta, avaliação e acolhimento, onde entendemos suas necessidades
-              e traçamos juntos o melhor caminho para cuidar do seu sorriso.
-            </p>
-            <aside>Venha florescer conosco!</aside>
-          </div>
+          <p>
+            A primeira consulta é o início dessa jornada: um momento de escuta,
+            avaliação e acolhimento, onde entendemos suas necessidades e
+            traçamos juntos o melhor caminho para cuidar do seu sorriso.
+          </p>
+          <aside>Venha florescer conosco!</aside>
+        </div>
       </section>
 
       <div className="container-consulta">
-          <p>ONLINE OU POR TELEFONE</p>
-          <h1>MARQUE JÁ A SUA CONSULTA</h1>
-        </div>
+        <p>ONLINE OU POR TELEFONE</p>
+        <h1>MARQUE JÁ A SUA CONSULTA</h1>
+      </div>
 
       <div className="consulta-flore">
-        <Button variant={"nav"} isActive={false}>
-          Marque aqui!
-        </Button>
+        <Link to={"consulta"}>
+          <Button variant={"nav"} isActive={false}>
+            Marque aqui!
+          </Button>
+        </Link>
       </div>
 
       <Banner variant={"withBg"}>
@@ -75,16 +79,15 @@ function App() {
           title={"Conheça os nossos dentistas!"}
           info={nossosDentistas}
         />
+        <Link to={"corpo-clinico"}>
         <Button variant={"nav"} isActive={false}>
           Saiba mais!
         </Button>
+        </Link>
       </Banner>
 
       <Banner variant={"withoutBg"}>
-        <InsideBanner
-          title={"A nossa clínica"}
-          info={nossaClinica}
-        />
+        <InsideBanner title={"A nossa clínica"} info={nossaClinica} />
       </Banner>
 
       <section className="container-mapa">
