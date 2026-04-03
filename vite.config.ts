897ce8d6697d469330f0a4c8bbcb4e34cfba780f
stephaniebrandao <salbuquerque.md@gmail.com>
2026-04-03 21:00:17 +0100

    refactor: change the const values in supabaseClient and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { loadEnv } from "vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), ""); // Carregar variáveis de ambiente
+
+  return {
+    plugins: [react()],
+    define: {
+      VITE_GOOGLE_MAPS_API_KEY: JSON.stringify(env.VITE_GOOGLE_MAPS_API_KEY),
+      SUPABASE_URL: JSON.stringify(env.SUPABASE_URL),
+      SUPABASE_ANON_KEY: JSON.stringify(env.SUPABASE_ANON_KEY)
+    },
+  };
+});
