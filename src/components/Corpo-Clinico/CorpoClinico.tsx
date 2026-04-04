0c794b66cc7ef7aa4bdd923866b530ca2b0ecd5b
stephaniebrandao <salbuquerque.md@gmail.com>
2026-04-05 00:34:52 +0100

    refactor: change the name of the CSS file, change the import module and update the classnames
    feat: add useEffect, useState and Card component to show the information about the professionals who work at the clinic

--- src/components/Corpo-Clinico/CorpoClinico.tsx
+++ src/components/Corpo-Clinico/CorpoClinico.tsx
@@ -1,5 +1,55 @@
-function CorpoClinico () {
-    return <h1>Corpo Clínico</h1>
+import { useEffect, useState } from "react";
+import Header from "../Header/Header";
+import styles from "./corpoClinico.module.css";
+import type { ICorpoClinico } from "../../types";
+import { supabase } from "../../supabaseClient";
+import Card from "../Card/Card";
+
+function CorpoClinico() {
+  const [corpoClinico, setCorpoClinico] = useState<ICorpoClinico[]>([]);
+
+  useEffect(() => {
+    const fetchCorpoClinico = async () => {
+      const { data: newCorpoClinico, error } = await supabase
+        .from("profissionais")
+        .select();
+
+      if (error) console.error("Error fetching: ", error);
+      console.log("newCorpoClinico", newCorpoClinico);
+      setCorpoClinico(newCorpoClinico || []);
+    };
+
+    fetchCorpoClinico();
+  }, []);
+
+  const subtitle =
+    "Conheça os especialistas da Flore que unem técnica avançada e um olhar atento para transformar o seu sorriso!";
+
+  return (
+    <>
+      <Header
+        title={"Gente que cuida de gente"}
+        subtitle={subtitle}
+        variant={"first"}
+      />
+
+      <div className={styles.corpoClinicoGrid}>
+        {corpoClinico.map((profissional) => (
+          <Card
+            image={
+              <img
+                style={{ width: "120px", height: "170px", borderRadius: "50%" }}
+                src={profissional.imagens}
+              />
+            }
+            title={profissional.nome}
+            subtitle={profissional.especialidade}
+            variant={"cclinico"}
+          />
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default CorpoClinico;
