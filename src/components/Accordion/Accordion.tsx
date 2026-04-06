6d58396ecede03dd607af0d12a90a08fcfc2ce97
stephaniebrandao <salbuquerque.md@gmail.com>
2026-04-06 17:52:03 +0100

    feat: add type to the IAccordion interface property

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,7 +1,14 @@
 import AccordionItem from "./AccordionItem";
 
+export type AccordionData = {
+  title: string,
+  text: string,
+  listOne?: string,
+  listTwo?: string,
+  listThree?: string
+}
 export interface IAccordion {
-    data: any[];
+    data: AccordionData[];
 }
 
 function Accordion({ data }: IAccordion) {
@@ -9,8 +16,8 @@ function Accordion({ data }: IAccordion) {
   return (
     <>
     <div>
-        {data.map((el, num) => {
-            return <AccordionItem title={el.title} key={num}>
+        {data.map((el) => {
+            return <AccordionItem title={el.title}>
                 <p>{el.text}</p>
                 <p>{el.listOne}</p>
                 <p>{el.listTwo}</p>
