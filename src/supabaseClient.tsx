e1fcc7c38d68512a2d3fd191a0e5ea8fe1714761
stephaniebrandao <salbuquerque.md@gmail.com>
2026-04-10 16:51:05 +0100

    fix: correct warning message for missing Supabase keys

--- src/supabaseClient.tsx
+++ src/supabaseClient.tsx
@@ -4,7 +4,7 @@ const supabaseUrl = "https://qjphwxkhigllyjpmqsri.supabase.co";
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqcGh3eGtoaWdsbHlqcG1xc3JpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4NzUyNzgsImV4cCI6MjA5MDQ1MTI3OH0.tjTIgWZLJYC3O7CWhuHBS5P8KyAQhiHH1VWDN6d3Rh0";
 
 if (!supabaseUrl || !supabaseAnonKey) {
-    console.warn("Atenção: Chaves do Supabase não encontradas!");
+    console.warn("Atenção: Chaves do Supabase não foram encontradas!");
   }
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
